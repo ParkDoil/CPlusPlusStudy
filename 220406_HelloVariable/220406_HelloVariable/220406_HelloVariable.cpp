53671081f561cd84bf9aa68cb95bbf7a6cd05da9
ParkDoil <poker5323@naver.com>
2022-04-07 16:53:47 +0900

    220407_Add_연산자수업
    4월 7일 공부 내용 총 정리본
    1)변수
    2) 연산자
     2.1) 산술연산자
     2.2) 대입연산자
     2.3) 복합대입연산자
     2.4) 증감연산자
     2.5) 관계연산자

--- 220406_HelloVariable/220406_HelloVariable/220406_HelloVariable.cpp
+++ 220406_HelloVariable/220406_HelloVariable/220406_HelloVariable.cpp
@@ -23,15 +23,15 @@ int main()
     /*
         1) 이름 : testNum    2) 크기 : 4Byte    3) 값 : 2.8    4) 메모리주소 : 000000F9A7AFF694
     */
-    //float testNum = 2.8f; // 부동소수점 7자리까지 표현 가능
-    //double testNum1 = 2.8; // 부동소수점 15자리까지 표현 가능
-    //// 기본적으로 실수를 입력하면 컴퓨터는 double형으로 인식
-    //// double형은 부동소수점 15자리까지 가능하기때문에 상대적으로 크기가 작은 float에 대입하려고 하면 오류 발생
-    //// 따라서 숫자 뒤에 f를 붙혀 float형이라는걸 표기해야한다.
+    float testNum = 2.8f; // 부동소수점 7자리까지 표현 가능
+    double testNum1 = 2.8; // 부동소수점 15자리까지 표현 가능
+    // 기본적으로 실수를 입력하면 컴퓨터는 double형으로 인식
+    // double형은 부동소수점 15자리까지 가능하기때문에 상대적으로 크기가 작은 float에 대입하려고 하면 오류 발생
+    // 따라서 숫자 뒤에 f를 붙혀 float형이라는걸 표기해야한다.
 
-    //cout << testNum << endl;
-    //cout << &testNum << endl; // 메모리주소 위치 확인
-    //cout << sizeof(testNum) << "Byte" << endl; // 데이터타입의 크기 확인
+    cout << testNum << endl;
+    cout << &testNum << endl; // 메모리주소 위치 확인
+    cout << sizeof(testNum) << "Byte" << endl; // 데이터타입의 크기 확인
 
     // 실습 1. 상대방 이름과 내 이름, 상대방 나이를 입력 받아서 생일 축하메시지를 출력하는 프로그램을 만들어보자.
     // -> 1. 상대방의 이름과 내이름, 상대방 나이를 입력 받는다.
@@ -88,7 +88,7 @@ int main()
             int Age;
             int aGe;
             int agE;
-        
+
         4. 변수명 사이에 띄어쓰기를 넣을 수 없다.
             -> 띄어쓰기는 다른 요소로 분류
             ex) int my age = 24 (X)
@@ -107,7 +107,7 @@ int main()
             char chLetter;
             bool bCheck;
             string strName;
-        
+
 
             -카멜 표기법 예시-
             int myAge;
@@ -141,4 +141,117 @@ int main()
             MBTI : ?
             ....
     */
+
+
+    /*
+        <<연산자>>
+        - 데이터를 가공(연산)하는 모든 명령에 필요한 것들
+        1) 어떤종류의 연산자들이 있는지
+        2) 우선순위 : 어떤 순서로 연산이 진행되는지
+        3) 결합순서 : 좌측부터 연산하는 연산자, 우측부터 연산하는 연ㅅ나자.
+
+        1. 산술연산자 : 사칙연산 (좌측부터 연산)
+            + : 더하기
+            - : 뺴기
+            * : 곱하기
+            / : 나누기     11 / 3 = 3
+            % : 나머지     11 % 3 = 2
+            우선순위 -> *, /, %  > +, -
+
+        2. 대입연산자 (우측부터 연산)
+            = : 왼쪽 항에 오른쪽 항의 값을 대입한다.(복사한다)
+
+        3. 복합대입연산자
+            += : a += b; -> a = a + b;
+            -= : a -= b; -> a = a - b;
+            *= : a *= b; -> a = a * b;
+            /= : a /= b; -> a = a / b;
+            %= : a %= b; -> a = a % b; (a와 b는 정수형 타입이어야 한다)
+            -> 변수가 정수형일때는 b가 0이 아니면 큰 오류가 없다
+            -> 값을 바꾸기 위해서 대입 연산자를 사용한다.
+
+        4. 증감연산자 : 특정 변수의 값을 1씩 증가시키거나 감소시킬 떄 사용한다.
+            -> 변수의 앞이나 뒤에 ++ / --를 붙혀서 사용한다.
+            -> 변수의 뒤에 증감연산자가 있을경우 문장을 종료한뒤에 증감연산자 실행
+                ex) left_value = 14; cout << left_value++ << endl; // 출력 : 14 , 메모리 : 15
+            -> 변수의 앞에 증감연산자가 있을경우 증감연산자 먼저 실행 후 문장 종료
+                ex) left_value = 14; cout << ++left_value << endl; // 메모리 : 15, 출력 : 15
+
+        5. 관계연산자 : 두 값을 비교한 결과가 참(1, true))인지 거짓(0, false)인지 반환하는 연산자
+            < : a < b : 왼쪽 항의 값이 오른쪽 항의 값보다 작으면 참, 아니면 거짓(크거나 같을때)
+            > : a > b : 왼쪽 항의 값이 오른쪽 항의 값보다 크면 참, 아니면 거짓(작거나 같을때)
+            <= : a <= b : 왼쪽 항의 값이 오른쪽 항의 값보다 작거나 같으면 참, 아니면 거짓(클때)
+            >= : a >= b : 왼쪽 항의 값이 오른쪽 항의 값보다 크거나 같으면 참, 아니면 거짓(작을때)
+            == : a == b : 왼쪽 항의 값이 오른쪽 항의 값과 같으면 참, 아니면 거짓(크거나 작을때)
+            != : a != b : 왼쪽 항의 값이 오른쪽 항의 값고 다르면 참, 아니면 거짓(같을때)
+    */
+
+
+    //산술연산자
+    int left_value = 14;
+    int right_value = 3; // 값을 0으로 바꾸면 *까지 출력지만 /, %는 런타임에러 발생
+
+    cout << "+ : " << left_value + right_value << endl;
+    cout << "- : " << left_value - right_value << endl;
+    cout << "* : " << left_value * right_value << endl;
+    cout << "/ : " << left_value / right_value << endl;
+    cout << "% : " << left_value % right_value << endl << endl;
+
+
+    //복합대입연산자
+    int left_value2 = 27;
+    int right_value2 = 5;
+    
+    cout << left_value2 << " += " << right_value2 << " => ";
+    left_value2 += right_value2;
+    cout << left_value2 << endl;
+
+    cout << left_value2 << " -= " << right_value2 << " => ";
+    left_value2 -= right_value2;
+    cout << left_value2 << endl;
+
+    cout << left_value2 << " *= " << right_value2 << " => ";
+    left_value2 *= right_value2;
+    cout << left_value2 << endl;
+
+    cout << left_value2 << " /= " << right_value2 << " => ";
+    left_value2 /= right_value2;
+    cout << left_value2 << endl;
+
+    cout << left_value2 << " %= " << right_value2 << " => ";
+    left_value2 %= right_value2;
+    cout << left_value2 << endl << endl;
+
+    //증감연산자
+    //left_value++; //15
+    cout << left_value++ << endl; // 출력 : 14 , 메모리 : 15
+    //++left_value; //16
+    cout << ++left_value << endl; // 메모리 : 16, 출력 : 16;
+
+    cout << "초기 값 : " << left_value << endl;
+    cout << "출력문 안 변수앞에 증감연산자 : " << --left_value << endl; //메모리 : 15, 출력 : 15
+    cout << "출력문 안 변수뒤에 증감연산자 : " << left_value-- << endl; //출력 : 15, 메모리 :14
+    left_value--; // 증감연산자로 인해 13
+    cout << "증감연산자(변수뒤) 먼저 한 뒤에 출력 : " << left_value << endl;
+    --left_value; // 증감연산자로 인해 12
+    cout << "증감연산자(변수앞) 먼저 한 뒤에 출력 : " << left_value << endl << endl;
+
+    /*
+        실습.
+        1. 고객의 나이를 입력받아서 미성년자(19세 이하)이면 true를 출력, 성인이면 false를 출력.
+        2. 입력받은 나이를 기준으로 주민등록증 발급대상(20살이상)이면 true를 아니면 false를 출력.
+        + 3. 아홉수(1의자리가 9인수)인지 확인해서 true, false를 출력
+    */
+
+    int cinAge;
+
+    cout << "나이를 입력하시오 : ";
+    cin >> cinAge;
+
+    cout << "┌────────────────────────────────────────────────────────┐" << endl;
+    cout << "│ 미성년자 맞죠?? >> \t\t\t" << boolalpha << (cinAge <= 19) << "\t\t │" << endl;
+    cout << "│ 주민등록증 발급대상인가요?? >> \t" << boolalpha << (cinAge == 20) << "\t\t │" << endl;
+    cout << "│ 아홉수인가요?? >> \t\t\t" << boolalpha << ((cinAge % 10) == 9) << "\t\t │" << endl;
+    cout << "└────────────────────────────────────────────────────────┘" << endl;
+
 }
