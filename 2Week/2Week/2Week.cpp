a8e81f357894f4f972a1b2a16d66b208b841aa03
ParkDoil <poker5323@naver.com>
2022-04-12 17:41:46 +0900

    220412_commit

--- 2Week/2Week/2Week.cpp
+++ 2Week/2Week/2Week.cpp
@@ -1,275 +1,665 @@
-﻿// C++ 문법
-
-// 주석 : 실행(컴파일)되지 않고 설명만을 위해서 사용되는 문법
-
-/*
-	1. 문단 자체를
-	2. 주석처리 할 때
-	3. 사용하는 주석
-*/
-
-/*
-	C++ 문법을 사용할 때 주의해야 할 점
-	1. 대/소문자를 구분해야한다.
-	main / Main / mAin / maIN 전부 다 다르다.
-
-	2. 띄어쓰기를 통해 요소를 구분한다.
-
-	3. 실행문의 마무리는 세미콜론(;)으로 한다.
-
-	[변수]
-	데이터타입 변수명;
-	데이터타입 변수명 = 10;
-	데이터타입 변수명 = 'ㅁ';
-	데이터타입 변수명 = "Hello";
-
-	데이터타입
-	1. 정수형 (0, 1, 2 ..., -1, -2 ...)
-		int
-		short
-		long
-		long long
-	2. 실수형
-		float
-		double
-		long double
-	3. 문자형
-		char
-	4. 문자열
-		string
-
-	변수 이름 짓기 규칙(표기법)
-	1. 헝가리안 표기법
-	 - 데이터 타입 약자를 접두어로 붙인다.
-	 int => int nMaxCount = 10;
-	2. 카멜 표기법
-	 - 두개 이상의 단어로 이루어진 변수명을 두번째 단어 이후 첫번째글자는 대문자로 표기
-	 int maxCount = 10;
-	3. 파스칼 표기법
-	 - 첫번째 단어 첫번째 글자부터 대문자로 표기
-	 int MaxCount = 10;
-
-	int nn = 10;
-	int cmprs =20; 처럼 약자를 사용하여 표기하는건 안좋은 버릇임
-
-	int token_max_count = 10; <- 스네이크 표기법
-
-	1. 대소문자를 구분한다.
-	2. 알파벳, 숫자, _(언더바)만 사용한다.
-	 2-1. 숫자는 가장 앞에 올 수 없다.
-
-	변수의 4가지 속성
-	1) 이름 : input_key		2) 크기 : 4Byte		3) 값 : 10		4) 메모리주소 : 0000002B818FF674 (cout << &input_key << endl;를 출력했을때 나오는 값)
-
-	연산자 : 데이터를 가공(연산)하는 모든 명령에 필요한 것들
-	1) 어떤 종류의 연산자인가
-	2) 우선순위 : 어떤 순서로 연산이 되는가
-	3) 결합순서 : 좌측부터 연산하는가, 우측부터 연산하는가
+﻿/*
+	제어문		데이터(타입) / 연산자 / <제어구조>
 	
-	1. 산술연산자(사칙연산) : 좌측부터 연산된다
-	+ : 더하기
-	- : 빼기
-	* : 곱하기
-	/ : 나누기		11 / 3 -> 3
-	% : 나머지		11 % 3 -> 2
-	우선순위 : *, /, % > +, -
+	1. 조건문 : 특정 조건에 따라서 실행문의 실행여부를 제어 => if문 / switch문
+	2. 반복문 : 특정 조건에 따라서 실행문을 일정 횟수만큼 반복 실행 제어 => for문 / while문
 
-	2. 대입 연산자 : 우측부터 연산된다
-	= : 오른쪽 변수의 값을 왼쪽 변수에 대입
-	left_num = right_num -> right_num 값을 left_num 값에 대입
+	<조건문>
+	1.1) if 문
+	if (조건식(참/거짓)) {
+		실행문;
+	}
+	else if (조건식2(참/거짓)) {
+		실행문2;
+	}
+	else {
+		실행문3;
+	}
 
-	3. 복합대입연산자
-	+= : 오른쪽 값에 왼쪽 값을 더한 값을 왼쪽 변수에 대입
-	a += b; -> a = a + b;
-	-= : 오른쪽 값에 왼쪽 값을 뺀 값을 왼쪽 변수에 대입
-	a -= b; -> a = a - b;
-	*= : 오른쪽 값에 왼쪽 값을 곱한 값을 왼쪽 변수에 대입
-	a *= b; -> a = a * b;
-	/= : 오른쪽 값에 왼쪽 값을 나눈 값을 왼쪽 변수에 대입
-	a /= b; -> a = a / b; (b == 0, 런타임에러 발생)
-	%= : 오른쪽 값에 왼쪽 값을 나눈 나머지 값을 왼쪽 변수에 대입
-	a %= b; -> a = a % b; (데이터타입이 정수형이 아닐때 에러 발생)
+	1.2) switch 문
+	switch(정수형 변수) {
+		case 정수1 :
+			    실행문1;
+			    break;
+			.
+			.
+			.
+		case 정수n :
+			    실행문n;
+			    break;
+		default: // 해당하는 정수 값이 없을때
+				실행문d;
+				break;
+	}
 
-	4. 증감 연산자 : 변수 앞 또는 뒤에 붙어 사용하며 해당 변수의 값을 1씩 증가시키거나 감소시킬때 사용 
-	int variable = 10;
-	++ : 1씩 증가
-	cout << ++variable << endl; 실행 -> 출력값 : 11, 메모리 : 11
-	cout << variable++ << endl; 실행 -> 출력값 : 11, 메모리 : 12
-	-- : 1씩 감소
-	cout << --variable << endl; 실행 -> 출력값 : 11, 메모리 : 11
-	cout << variable-- << endl; 실행 -> 출력값 : 11, 메모리 : 10
+	<반복문>
+	2.1.1) while문
+	while (조건식){
+		실행문;
+		조건식을 바꾸는 실행문; // while문을 중단시키는 첫번째 방법
+		if(조건식){
+			break;			  // while문을 중단시키는 두번째 방법
+		}
+	}
 
-	5. 관계 연산자 : 두 항의 값을 비교한 결과가 참(1, true) 또는 거짓(0, false)으로 반환되는 연산자
-	a < b : a가 b보다 작으면 1, 크거나 같으면 0 반환
-	a > b : a가 b보다 크면 1, 작거나 같으면 0 반환
-	a <= b : a가 b보다 작거나 같으면 1, 크면 0 반환
-	a >= b : a가 b보다 크거나 같으면 1, 작으면 0 반환
-	a == b : a와 b가 같으면 1, 다르면 0 반환
-	a != b : a와 b가 다르면 1, 같으면 0 반환
+	2.1.2) do-while문
+	do {
+		실행문;
+	}
+	while (조건식);
 
-	6. 논리 연산자 : 왼쪽 항과 오른쪽 항의 값을 각각 참과 거짓으로 연산해서 두 값을 비교해서 참과 거짓을 반환하는 연산자
-	&& : 논리곱(AND)
-	a && b : a의 결과 값이 참이고, b의 결과값이 참이면 1을 반환, 그 외에는 0을 반환
-	|| : 논리합(OR)
-	a || b : a의 결과 값이 참이거나, b의 결과값이 참이면 1을 반환, 둘다 거짓일때만 0을 반환
-	! : 논리부정(NOT)
-	!a : a의 결과값이 참이면 거짓을 반환, 거짓이면 참을 반환
-
-	7. 삼항연산자
-	a ? b : c;
-	a의 결과 값이 참이면 b실행문 동작, 거짓이면 c실행문이 동작
-
-	난수 (랜덤값)
-	rand(); // 0 ~ 32767 사이의 랜덤한 값이 반환된다
-	함수 내부에 seed값이 있어서 이 값에 의해서 리턴값이 정해진다
-	기본적으로 seed : 1로 세팅되어 있다.
-	srand(time(NULL)); -> 시드값을 실행할때마다 다시 세팅
-	*/
-
-/*
-	실습1. 3 ~ 7 사이의 랜덤한 숫자를 출력하는 프로그램을 작성하자
-	(3, 7 포함)
-
-	실습2. 가장 작은 값과 큰 값을 입력 받아서, 그 사이의 임의의 값을 출력하는 프로그램을 작성하자
-
-	실습3. 프로그램을 실행하면 내부적으로 1~9사이의 숫자가 정답으로 정해지고, 유저가 1 ~ 10 사이의 숫자를 입력하면 맞으면 true, 틀리면 false를 출력
+	2.2) for문
+	for((1)초기식; (2)조건식; (3)증감식){
+		(4)실행문;
+	}
+	=> 순서 : (1) -> (2) -> (4) -> (3) -> (2) -> (4) -> (3) -> ... 조건식의 결과값이 0이 나올 때 까지
 */
 
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
+	srand(time(NULL));
+	//실습 1번 3 ~ 7 사이의 랜덤한 숫자 하나 출력
+	int rand_num;
+	//입력 (X)
+	
+	//처리
+	rand_num = rand() % 5 + 3;
+	//출력
+	cout << "3~7사이 랜덤한 숫자 : " << rand_num << endl;
 
-	//1주차 복습
-	int inputKey;
-	float testFloat = 5.3f;
-	float testFloat2 = 5.3;
-	char testCharacter = 'a';
-	string testString = "Hello";
+	//실습 2번 작은 수와 큰 수를 입력받아 두 수 사이의 랜덤한 숫자 하나 출력
+	//입력
+	int input_min_num;
+	int input_max_num;
+	cout << "작은 수 : ";
+	cin >> input_min_num;
+	cout << "큰 수 : ";
+	cin >> input_max_num;
+	//처리
+	rand_num = rand() % (input_max_num - input_min_num + 1) + input_min_num;
+	//출력
+	cout << "사이의 랜덤한 숫자 : " << rand_num << endl;
 
-	cout << "Hello World!\n";
-	cin >> inputKey;
+	//실습 3번 프로그램을 실행하면 1 ~ 9 사이의 숫자가 정답으로 정해진다. 유저가 1 ~ 9 사이의 숫자를 입력하면 맞으면 true, 틀리면 false를 출력
+	//처리
+	int correct_num = rand() % 9 + 1;
+	cout << "미리보는 정답 : " << correct_num << endl;
+	//입력
+	int input_quiz_num;
+	cin >> input_quiz_num;
+	//출력
+	(input_quiz_num == correct_num) ? cout << "True" << endl : cout << "False" << endl;
 
-	// 증감 연산자
-	int variable = 10;
-	cout << variable++ << endl;
-	cout << ++variable << endl;
-	cout << variable-- << endl;
-	cout << --variable << endl;
+	/*
+		if 문 실습.
+		입력받은 숫자가 10보다 크면 "입력된 숫자가 10보다 큽니다." 출력
+		10이하 9초과 이면 "9보다 큽니다." 출력
+		9이하 6초과 이면 "6보다 큽니다." 출력
+		6이하 이면 "6보다 작습니다." 출력
+		
+	*/
+	//입력
+	int input_num;
+	cout << "숫자 하나를 입력하시오 : ";
+	cin >> input_num;
+	//처리, 출력
+	if (input_num > 10) {
+		cout << "입력된 숫자는 10보다 큰 수 입니다." << endl;
+	}
+	else if (input_num > 9) {
+		cout << "입력된 숫자는 9보다 큰 수 입니다." << endl;
+	}
+	else if (input_num > 6) {
+		cout << "입력된 숫자는 6보다 큰 수 입니다." << endl;
+	}
+	else {
+		cout << "입력된 숫자는 6보다 작거나 같은 수 입니다." << endl;
+	}
+	/*
+		swtich 문 실습.
+		당신의 고향은 어디입니까?
+		1. 서울		2. 양평		3. 광주		4. 대전		5. 제주도
+		_ (입력받기)
 
-	// 관계 연산자
-	cout << (10 > 5) << endl; // 1
-	cout << (10 < 5) << endl; // 0
-	cout << (10 >= 5) << endl; // 1
-	cout << (10 <= 5) << endl; // 0
-	cout << (10 == 5) << endl; // 0
-	cout << (10 != 5) << endl; // 1
+		서울 => 안녕하세요. 당신의 고향은 서울이군요.
+		제주도 => 안녕하수꽝. 혼저옵서예.
+		5개 중에 선택해주세요.
+	*/
+	//입력
+	int hometown_num;
+	cout << "당신의 고향은 어디입니까?" << endl << "1.서울  2.대전  3.전주  4.부산  5.제주도" << endl << "_ ";
+	cin >> hometown_num;
+	
+	//처리, 출력
+	switch (hometown_num) {
+	case 1:
+		cout << "안녕하세요. 당신의 고향은 서울이군요." << endl;
+		break;
+	case 2:
+		cout << "안녕하세유. 당신 고향 대전이군요." << endl;
+		break;
+	case 3:
+		cout << "안녕하시랑께. 당신의 고향은 전주군요." << endl;
+		break;
+	case 4:
+		cout << "안녕하시꽝. 당신의 고향은 부산이군요." << endl;
+		break;
+	case 5:
+		cout << "혼저옵서예. 제주도 사람인?" << endl;
+		break;
+	default:
+		cout << "5개 중에 하나를 선택하세요." << endl;
+		break;
+	}
+	// 위 switch 문 if문으로 변경
+	if (hometown_num == 1) {
+		cout << "안녕하세요. 당신의 고향은 서울이군요." << endl;
+	}
+	else if (hometown_num == 2) {
+		cout << "안녕하세유. 당신 고향 대전이군요." << endl;
+	}
+	else if (hometown_num == 3) {
+		cout << "안녕하시랑께. 당신의 고향은 전주군요." << endl;
+	}
+	else if (hometown_num == 4) {
+		cout << "안녕하시꽝. 당신의 고향은 부산이군요." << endl;
+	}
+	else if (hometown_num == 5) {
+		cout << "혼저옵서예. 제주도 사람인?" << endl;
+	}
+	else {
+		cout << "5개 중에 하나를 선택하세요." << endl;
+	}
+	/*
+		while문 실습
+		Hello World를 5번 출력하는 프로그램
+	*/
+	int repeat_count = 0;
+	//while문 조건식 변경탈출
+	while (repeat_count < 5) {
+		cout << "1. Hello World!" << endl;
+		repeat_count++;
+	}
 
-	// 논리 연산자
-	cout << !- 300 << endl; //0
+	//while문 실행문 안에서 if문으로 break 탈출
+	cout << endl;
+	repeat_count = 0;
+	while (1) {
+		cout << "2. Hello World!" << endl;
+		repeat_count++;
 
-	// 삼항 연산자
-	(10 > 5) ? cout << "10이 5보다 크다" : cout << "10이 5보다 작다";
+		if (repeat_count >= 5) {
+			break;
+		}
+	}
+
+	//do-while문
+	cout << endl;
+	repeat_count = 0;
+	do {
+		cout << "3. Hello World!" << endl;
+		repeat_count++;
+	} while (repeat_count < 5);
+
+	// 실습3. 정답 맞추는 문제를 정답 맞출 때 까지 반복되는 프로그램으로 수정
+	cout << endl;
+	int correct_num = rand() % 9 + 1;
+	int input_quiz_num = 0;
+	while (input_quiz_num != correct_num) {
+		cout << "정답 일 것 같은 숫자 하나를 입력하시오 : _ ";
+		cin >> input_quiz_num;
+		(input_quiz_num == correct_num) ? cout << "정답! 프로그램 종료" << endl : cout << "오답!" << endl;
+	}
+	
+	/*
+		for문 
+		실습1. 10부터 1까지 차례대로 출력되는 코드를 작성해보자.
+		실습2. 100미만의 3의 배수를 차례대로 출력되는 코드 작성해보자
+		실습3. 1000미만의 2의 승수를 차례대로 출력
+		실습4. 구구단을 출력해보자.
+		4.1) 구구단 2단을 출력해보자.
+		4.2) 구구단 2단 ~ 9단을 차례대로 출력해보자
+		2단
+		3단
+		4단
+		4.3) 구구단 2단 ~ 9단을 다음과 같은 형태로 출력해보자
+		2단 3단 4단
+		5단 6단 7단
+		8단 9단
+	*/
+	cout << "<< 실습 1번 >>" << endl;
+	int question1_num = 10;
+	for (int i = 0; i < 10; i++) {
+		cout << " " << question1_num<< endl ;
+		question1_num--;
+	}
+
+	cout << "<< 실습 2번 >>" << endl;
+	for (int i = 1; i < 100; i++) {
+		if ((i % 3) == 0) {
+			cout << " " << i << " ";
+		}
+	}
+
+	cout << "<< 실습 3번 >>" << endl;
+	for (int i = 1; i < 1000; i*=2) {
+		cout << " " << i << endl;
+	}
+
+	//4.1) 구구단 2단을 출력해보자.
+	cout << "구구단 2단 출력" << endl;
+	int two_num = 2;
+	for (int i = 1; i <= 9; i++) {
+		cout << " " << two_num << " * " << i << " = " << two_num * i << endl;
+	}
 	cout << endl;
 
-	srand(time(NULL));
-	cout << rand() << endl;
-	cout << sizeof(rand()) << "Byte" << endl;
-
-	//스플랜더 변수 세팅하기
-	// 토큰 변수세팅
-	int white_token, black_token, red_token, blue_token, green_token, gold_token;
-	// 황금 토큰 제외 7개 세팅
-	white_token = black_token = red_token = blue_token = green_token = 7;
-	gold_token = 5; // 황금 토큰 5개 세팅
-
-	int card_id; //개발카드 id
-	// 카드 구매 비용
-	int cardcost_black;
-	int cardcost_white;
-	int cardcost_red;
-	int cardcost_blue;
-	int cardcost_green;
-	// 카드 할인 비용
-	int black_discount;
-	int white_discount;
-	int red_discount;
-	int blue_discount;
-	int green_discount;
-	int card_score; // 카드 점수				
-	// 카드 타입별 개수
-	int card_green = 40;
-	int card_yellow = 30;
-	int card_blue = 20;
-	string card_type; //카드 타입
-
-	string loyalcard_name; // 귀족 카드 이름
-	int loyalcard_score = 3; // 귀족 카드 점수
-	// 귀족카드 비용
-	int loyalcard_blackcost;
-	int loyalcard_greencost;
-	int loyalcard_whitecost;
-	int loyalcard_bluecost;
-	int loyalcard_redcost;
-	int loyalcard_count; // 귀족카드 개수
-
-	int player_id; // 플레이어id
-	// 플레이어 소지 보석
-	int have_white;
-	int have_green;
-	int have_red;
-	int have_blue;
-	int have_black;
-	int player_score; // 플레이어 점수
-	int player_haveID; // 플레이어 소지카드(개발카드id)
-	string have_loyalcard; // 플레이어가 소지한 귀족 타일
-	int booked_cardID; // 예약카드 id(개발카드id)
-	int booked_cardcount; // 예약카드 개수(최대 3개)
-	int player_behavior; // 플레이어 행동변수
-
-	int player_count; //플레이어 숫자
-	int now_turnID; //현재 턴 플레이어id
-	int win_grade = 15; //승리 점수
-	int player_havetoken; //플레이어 소지 토큰(10개 제한)
-
-	//실습 과제
-	srand(time(NULL)); // 시드값 초기화
-	int problem1_num; //실습 1번 문제 출력값
-	int problem2_input_minnum; //실습 2번 문제 작은값
-	int problem2_input_maxnum; //실습 2번 문제 큰값
-	int problem2_subtract_num; //실습 2번 작은 값과 큰 값 사이의 숫자 개수
-	int problem2_out_num; //실습 2번 숫자 사이의 랜덤한 출력값
-	int problem3_correct_num; // 실습 3번 정답 숫자
-	int problem3_input_num; // 실습 3번 입력 숫자
-
-	//번외 숙제
-	cout << endl << "번외 숙제 : 랜덤 함수 값의 데이터 크기 구하기" << endl;
-	cout << "랜덤 함수의 값의 데이터크기는" << sizeof(rand()) << "Byte 입니다." << endl << endl;
-
-	//실습 1번
-	cout << "<< 실습 1번 문제 >> " << endl;
-	problem1_num = rand() % 5 + 3;
-	cout << " 3 ~ 7 사이의 랜덤한 출력 값은 >> " << problem1_num << "입니다." << endl << endl;
-
-	//실습 2번
-	cout << "<< 실습 2번 문제 >> " << endl;
-	cout << "작은 값을 입력하시오 : ";
-	cin >> problem2_input_minnum;
-	cout << "큰 값을 입력하시오 : ";
-	cin >> problem2_input_maxnum;
-	problem2_subtract_num = problem2_input_maxnum - problem2_input_minnum + 1;
-	problem2_out_num = rand() % problem2_subtract_num + problem2_input_minnum;
-	cout << "작은 수 " << problem2_input_minnum << ", 큰 수 " << problem2_input_maxnum << " 사이의 랜덤한 숫자 출력 값은 >> " << problem2_out_num << "입니다." << endl << endl;
-
-	//실습 3번
-	cout << "<< 실습 3번 문제 >> " << endl;
-	problem3_correct_num = rand() % 9 + 1;
-	cout << "정답이라고 생각하는 숫자를 입력하시오 : ";
-	cin >> problem3_input_num;
-	(problem3_input_num == problem3_correct_num) ? cout << boolalpha << (problem3_input_num == problem3_correct_num) :
-		cout << boolalpha << (problem3_input_num == problem3_correct_num) << "  정답은 " << problem3_correct_num << "였습니다.";
+	//4.2) 구구단 2단 ~ 9단을 차례대로 출력해보자
+	cout << "구구단 2~9단 출력" << endl;
+	int gugudan1_num = 2;
+	while (gugudan1_num != 10) {
+		cout <<" <" << gugudan1_num << "단>" << endl;
+		for (int i = 1; i <= 9; i++) {
+			cout << " " << gugudan1_num << " * " << i << " = " << gugudan1_num * i << endl;
+		}
+		gugudan1_num++;
+		cout << endl;
+	}
+	
+	
+	/*4.3) 구구단 2단 ~ 9단을 다음과 같은 형태로 출력해보자
+		2단 3단 4단
+		5단 6단 7단
+		8단 9단
+		*/
+	cout << "구구단 형식에 맞춰 출력" << endl;
+	int mutiple_num = 1;
+	int gugu_su = 0;
+	cout << "2단\t\t" << "3단\t\t" << "4단\t\t" << endl;
+	while (1) {
+		for (gugu_su = 2; gugu_su <= 4; gugu_su++) {
+			cout << " " << gugu_su << " * " << mutiple_num << " = " << gugu_su * mutiple_num << "\t";
+		}
+		if (mutiple_num < 10) {
+			mutiple_num++;
+			cout << endl;
+		}
+		else if (mutiple_num == 10) {
+			break;
+		}
+	}
 	cout << endl << endl;
+	cout << "5단\t\t" << "6단\t\t" << "7단\t\t" << endl;
+	mutiple_num = 1;
+	while (1) {
+		for (gugu_su = 5; gugu_su <= 7; gugu_su++) {
+			cout << " " << gugu_su << " * " << mutiple_num << " = " << gugu_su * mutiple_num << "\t";
+		}
+		if (mutiple_num < 10) {
+			mutiple_num++;
+			cout << endl;
+		}
+		else if (mutiple_num == 10) {
+			break;
+		}
+	}
+	cout << endl << endl;
+	cout << "8단\t\t" << "9단\t\t" << endl;
+	mutiple_num = 1;
+	while (1) {
+		for (gugu_su = 8; gugu_su <= 9; gugu_su++) {
+			cout << " " << gugu_su << " * " << mutiple_num << " = " << gugu_su * mutiple_num << "\t";
+		}
+		if (mutiple_num < 10) {
+			mutiple_num++;
+			cout << endl;
+		}
+		else if (mutiple_num == 10) {
+			break;
+		}
+	}
+
+	/*
+		숙제 : 별찍기 6개
+		1) 
+		*
+		**
+		***
+		****
+		*****
+		
+		2)
+			*
+		   **
+		  ***
+		 ****
+		*****
+		
+		3)
+		*****
+		****
+		***
+		**
+		*
+		
+		4)
+		*****
+		 ****
+		  ***
+		   **
+		    *
+		
+		5)
+			*
+		   ***
+		  *****
+		 *******
+		*********
+		
+		6)
+		*********
+		 *******
+		  *****
+		   ***
+		    *
+
+		7)
+			*
+		   ***
+		  *****
+		 *******
+		*********
+		 *******
+		  *****
+		   ***
+			*
+		
+	*/
+	// 1)
+	cout << endl << "1번" << endl;
+	int memory1_num;
+	for (int star1 = 1; star1 < 6; star1++) {
+		memory1_num = star1;
+		while (star1 != 0) {
+			cout << "*";
+			star1--;
+		}
+		star1 = memory1_num;
+		cout << endl;
+	}
+	
+	// 2)
+	cout << endl << "2번" << endl;
+	int star_count2 = 1;
+	for (int star2 = 1; star2 < 6; star2++) {
+		if (star_count2 == 1) {
+			cout << "    *";
+		}
+		else if (star_count2 == 2) {
+			cout << "   **";
+		}
+		else if (star_count2 == 3) {
+			cout << "  ***";
+		}
+		else if (star_count2 == 4) {
+			cout << " ****";
+		}
+		else if (star_count2 == 5) {
+			cout << "*****";
+		}
+		star_count2++;
+		cout << endl;
+	}
+
+	// 3)
+	cout << endl << "3번" << endl;
+	int memory3_num;
+	int star_count3 = 5;
+	for (int star1 = 1; star1 < 6; star1++) {
+		memory3_num = star_count3;
+		while (star_count3 != 0) {
+			cout << "*";
+			star_count3--;
+		}
+		star_count3 = memory3_num-1;
+		cout << endl;
+	}
+
+	// 4)
+	cout << endl << "4번" << endl;
+	int star_count4 = 1;
+	for (int star4 = 1; star4 < 6; star4++) {
+		if (star_count4 == 1) {
+			cout << "*****";
+		}
+		else if (star_count4 == 2) {
+			cout << " ****";
+		}
+		else if (star_count4 == 3) {
+			cout << "  ***";
+		}
+		else if (star_count4 == 4) {
+			cout << "   **";
+		}
+		else if (star_count4 == 5) {
+			cout << "    *";
+		}
+		star_count4++;
+		cout << endl;
+	}
+
+	// 5)
+	cout << endl << "5번" << endl;
+	int star_count5 = 1;
+	for (int star5 = 1; star5 < 6; star5++) {
+		if (star_count5 == 1) {
+			cout << "    *    ";
+		}
+		else if (star_count5 == 2) {
+			cout << "   ***   ";
+		}
+		else if (star_count5 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count5 == 4) {
+			cout << " ******* ";
+		}
+		else if (star_count5 == 5) {
+			cout << "*********";
+		}
+		star_count5++;
+		cout << endl;
+	}
+
+	// 6)
+	cout << endl << "6번" << endl;
+	int star_count6 = 1;
+	for (int star6 = 1; star6 < 6; star6++) {
+		if (star_count6 == 1) {
+			cout << "*********";
+		}
+		else if (star_count6 == 2) {
+			cout << " ******* ";
+		}
+		else if (star_count6 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count6 == 4) {
+			cout << "   ***   ";
+		}
+		else if (star_count6 == 5) {
+			cout << "    *    ";
+		}
+		star_count6++;
+		cout << endl;
+	}
+
+	// 7)
+	cout << endl << "7번" << endl;
+	int star_count7 = 1;
+	for (int star7 = 1; star7 < 10; star7++) {
+		if (star_count7 == 1) {
+			cout << "    *    ";
+		}
+		else if (star_count7 == 2) {
+			cout << "   ***   ";
+		}
+		else if (star_count7 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count7 == 4) {
+			cout << " ******* ";
+		}
+		else if (star_count7 == 5) {
+			cout << "*********";
+		}
+		else if (star_count7 == 6) {
+			cout << " ******* ";
+		}
+		else if (star_count7 == 7) {
+			cout << "  *****  ";
+		}
+		else if (star_count7 == 8) {
+			cout << "   ***   ";
+		}
+		else if (star_count7 == 9) {
+			cout << "    *    ";
+		}
+		star_count7++;
+		cout << endl;
+	}
+	// 1)
+	cout << endl << "1번" << endl;
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		for (int k = 5; k > i; k--) {
+			cout << " ";
+		}
+		cout << endl;
+	}
+
+	// 2)
+	cout << endl << "2번" << endl;
+	for (int i = 0; i <= 5; i++) {
+		for (int k = 5; k > i; k--) {
+			cout << " ";
+		}
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	// 3)
+	cout << endl << "3번" << endl;
+	for (int i = 0; i <= 5; i++) {
+		for (int k = 5; k > i; k--) {
+			cout << "*";
+		}
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		cout << endl;
+	}
+	// 4)
+	cout << endl << "4번" << endl;
+	for (int i = 0; i <= 5; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		for (int k = 5; k > i; k--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	// 5)
+	cout << "5번" << endl;
+	int star_count5 = 1;
+	for (int star5 = 1; star5 < 6; star5++) {
+		if (star_count5 == 1) {
+			cout << "    *    ";
+		}
+		else if (star_count5 == 2) {
+			cout << "   ***   ";
+		}
+		else if (star_count5 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count5 == 4) {
+			cout << " ******* ";
+		}
+		else if (star_count5 == 5) {
+			cout << "*********";
+		}
+		star_count5++;
+		cout << endl;
+	}
+
+	// 6)
+	cout << endl << "6번" << endl;
+	int star_count6 = 1;
+	for (int star6 = 1; star6 < 6; star6++) {
+		if (star_count6 == 1) {
+			cout << "*********";
+		}
+		else if (star_count6 == 2) {
+			cout << " ******* ";
+		}
+		else if (star_count6 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count6 == 4) {
+			cout << "   ***   ";
+		}
+		else if (star_count6 == 5) {
+			cout << "    *    ";
+		}
+		star_count6++;
+		cout << endl;
+	}
+
+	// 7)
+	cout << endl << "7번" << endl;
+	int star_count7 = 1;
+	for (int star7 = 1; star7 < 10; star7++) {
+		if (star_count7 == 1) {
+			cout << "    *    ";
+		}
+		else if (star_count7 == 2) {
+			cout << "   ***   ";
+		}
+		else if (star_count7 == 3) {
+			cout << "  *****  ";
+		}
+		else if (star_count7 == 4) {
+			cout << " ******* ";
+		}
+		else if (star_count7 == 5) {
+			cout << "*********";
+		}
+		else if (star_count7 == 6) {
+			cout << " ******* ";
+		}
+		else if (star_count7 == 7) {
+			cout << "  *****  ";
+		}
+		else if (star_count7 == 8) {
+			cout << "   ***   ";
+		}
+		else if (star_count7 == 9) {
+			cout << "    *    ";
+		}
+		star_count7++;
+		cout << endl;
+	}
 }
